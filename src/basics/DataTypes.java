4bb0add6e77e45ed1d19518ec573954a16d234a1
itsmefreak <itsmefreak@tuta.io>
2022-03-24 13:11:04 +0100

    Data Types

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        char letter = 'A';
+        boolean done = false;
+        int radius = 10;
+        byte red = 127;
+        short age = 21;
+        long population = 313459000;
+        float price = 10.59f;
+        double circleArea = Math.PI*radius*radius;
+
+        /*
+        Strings are arrays of characters
+         */
+        String instructor = "Peggy Fisher";
+    }
+}
